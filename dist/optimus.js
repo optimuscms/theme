e16917b3df91a0e70f3e588f15e5055e2fa791b9
Rich Moore <rich@optixsolutions.co.uk>
2020-03-06 11:53:26 +0000

    Update vue select package

--- dist/optimus.js
+++ dist/optimus.js
@@ -1454,6 +1454,7 @@
 
 
         if (e.keyCode === 38) {
+          e.preventDefault();
           this.showPointer = false;
           var previousIndex = this.getPreviousFocusableIndex(this.focusedOptionIndex);
           this.setFocusedOption(this.options[previousIndex]);
@@ -1462,6 +1463,7 @@
 
 
         if (e.keyCode === 40) {
+          e.preventDefault();
           this.showPointer = false;
           var nextIndex = this.getNextFocusableIndex(this.focusedOptionIndex);
           this.setFocusedOption(this.options[nextIndex]);
